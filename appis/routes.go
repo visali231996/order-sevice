d882e5881066535a54c5e57c12f7620ae8df3a2e
visali231996 <visali231996@gmail.com>
2025-09-11 13:25:48 +0530

    created main file

---
+++ appis/routes.go
@@ -0,0 +1,13 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/IBM/sarama"
+)
+
+func RegisterRoutes(db *sql.DB, producer sarama.SyncProducer) {
+	h := NewHandler(db, producer)
+	http.HandleFunc("/create", h.CreateHandler())
+}
